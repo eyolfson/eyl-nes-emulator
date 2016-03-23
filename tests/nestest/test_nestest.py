e63b524d542be81d937079e986ce1b5cd11f34dd
Jon Eyolfson <jon@eyl.io>
2016-03-23 17:11:21 -0400

    Finish nestest script

--- tests/nestest/test_nestest.py
+++ tests/nestest/test_nestest.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 REQUIRED_FILES = [
@@ -8,6 +9,7 @@ REQUIRED_FILES = [
 ]
 
 def check_files():
+	checks_passed = True
 	for filename, sha256sum in REQUIRED_FILES:
 		completed_process = subprocess.run(["sha256sum", filename],
 		                                   stdout=subprocess.PIPE)
@@ -16,6 +18,52 @@ def check_files():
 			print("PASS")
 		else:
 			print("FAIL")
+			checks_passed = False
+	return checks_passed
+
+def check_build():
+	os.makedirs("build", exist_ok=True)
+	try:
+		subprocess.run(["cmake", "../src"], cwd="build", check=True)
+		subprocess.run(["make"], cwd="build", check=True)
+	except subprocess.CalledProcessError:
+		return False
+	return True
+
+def check_line(expected_line, actual_line):
+	CHECKS = [
+		("PC", 0, 4),
+		("A", 50, 52),
+		("X", 55, 57),
+		("Y", 60, 62),
+		("P", 65, 67),
+		("S", 71, 73),
+	]
+	for check in CHECKS:
+		expected = expected_line[check[1]:check[2]]
+		actual = actual_line[check[1]:check[2]]
+		if expected != actual:
+			print()
+			print("{} mismatched, expected: {}, actual: {}".format(
+				check[0], expected.decode(), actual.decode()))
+			return False
+	return True
+
+def run_test():
+	lines_passed = 0
+	completed_process = subprocess.run(["build/nes-emulator",
+	                                    "nestest.nes"],
+	                                   stdout=subprocess.PIPE)
+	lines = completed_process.stdout.splitlines()
+	with open("nestest.log", "rb") as f:
+		for line in f:
+			if not check_line(line, lines[lines_passed]):
+				return lines_passed
+			lines_passed += 1
+	return lines_passed
 
 if __name__ == "__main__":
-	check_files()
+	if check_files() and check_build():
+		lines_passed = run_test()
+		print()
+		print("{}/8991 lines passed".format(lines_passed))
