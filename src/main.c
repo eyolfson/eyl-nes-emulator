a4c4cb2673ce990e0d9a6c8f1cfd34c04797e230
Jon Eyolfson <jon@eyl.io>
2016-03-23 16:04:30 -0400

    Remove unneeded includes

--- src/main.c
+++ src/main.c
@@ -20,12 +20,7 @@
 #include "cpu.h"
 #include "prg_rom.h"
 
-#include <fcntl.h>
-#include <stdint.h>
-#include <string.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <unistd.h>
+#include <string.h> // TODO: remove after refactor
 
 void init()
 {
