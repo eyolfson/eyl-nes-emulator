1ffe8b9ca5d835c459b74010ba16b42848ce0d2e
Jon Eyolfson <jon@eyl.io>
2016-07-11 21:14:11 -0400

    Play random sound

--- src/backend/alsa.c
+++ src/backend/alsa.c
@@ -16,19 +16,52 @@
 
 #include "alsa.h"
 
+#include "../exit_code.h"
+
 #define ALSA_PCM_NEW_HW_PARAMS_API
 
 #include <alsa/asoundlib.h>
 
 static snd_pcm_t *handle;
 
+unsigned char buffer[16*1024];
+
 uint8_t alsa_init()
 {
 	int ret;
 	ret = snd_pcm_open(&handle, "default", SND_PCM_STREAM_PLAYBACK, 0);
-	if (ret < 0) {
-		return 1;
+	if (ret != 0) {
+		return EXIT_CODE_OS_ERROR_BIT;
 	}
+
+	ret = snd_pcm_set_params(
+		handle,
+		SND_PCM_FORMAT_U8,
+		SND_PCM_ACCESS_RW_INTERLEAVED,
+		1,     /* Number of PCM channels */
+		48000, /* Sample rate (Hz) */
+		1,     /* Allow resampling */
+		500000 /* Required letency (us) */
+	);
+	if (ret != 0) {
+		return EXIT_CODE_OS_ERROR_BIT;
+	}
+
+	snd_pcm_sframes_t frames;
+	for (unsigned int i = 0; i < 16; i++) {
+		frames = snd_pcm_writei(handle, buffer, sizeof(buffer));
+		if (frames < 0)
+			frames = snd_pcm_recover(handle, frames, 0);
+		if (frames < 0) {
+			printf("snd_pcm_writei failed: %s\n",
+				snd_strerror(frames));
+			break;
+		}
+		if (frames > 0 && frames < (long)sizeof(buffer))
+			printf("Short write (expected %li, wrote %li)\n",
+				(long)sizeof(buffer), frames);
+	}
+
 	return 0;
 }
 
